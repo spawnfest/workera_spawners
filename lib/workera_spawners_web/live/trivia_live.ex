7a069b0ef1c82c7a50c8e434e2afbd362614b431
dfnino10 <df.nino10@uniandes.edu.co>
2023-10-29 14:07:57 -0400

    add template for trivia game

--- lib/workera_spawners_web/live/trivia_live.ex
+++ lib/workera_spawners_web/live/trivia_live.ex
@@ -1,17 +1,54 @@
 defmodule WorkeraSpawnersWeb.TriviaLive do
-  use Phoenix.LiveView
+  use WorkeraSpawnersWeb, :live_view
+
+  alias WorkeraSpawners.GameServer
+
+  def mount(_params, _session, socket) do
+    server = GameServer.start_link(__MODULE__)
+    {
+      :ok,
+      socket
+      |> assign(
+      server: server,
+      game_state: :awaiting_name,
+      name: nil,
+      question: nil
+      )
+    }
+  end
 
   def render(assigns) do
-    ~L"""
-    <div>
-      <h1>Trivia Game</h1>
-      <p>Question: <%= @question %></p>
-      <p>Answer: <%= @answer %></p>
-    </div>
+    ~H"""
+    <%= case @game_state do %>
+      <% :awaiting_name -> %>
+        <%= form_for :name_form, "#", [phx_submit: :save_name], fn f -> %>
+          <%= text_input f, :name, placeholder: "Enter your name" %>
+          <%= submit "Submit" %>
+        <% end %>
+      <% :awaiting_players -> %>
+        <!-- Waiting for server event... -->
+      <% :finished -> %>
+        <!-- Finished game... -->
+      <% _ -> %>
+        <%= form_for :question_form, "#", [phx_submit: :save_answer], fn f -> %>
+          <label><%= @question %></label>
+          <%= text_input f, :answer, placeholder: "Enter your answer" %>
+          <%= submit "Submit" %>
+        <% end %>
+    <% end %>
     """
   end
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, question: "What is the capital of France?", answer: "Paris")}
+  def handle_event("save_name", %{"name_form" => %{"name" => name}}, %{assigns: %{server: s}} = socket) do
+    {:noreply, assign(socket, :name, name)}
+  end
+
+  def handle_event("save_answer", %{"question_form" => %{"answer" => answer}}, socket) do
+    # Handle answer submission
+    {:noreply, socket}
+  end
+
+  def handle_info({:question, question}, state, socket) do
+    {:noreply, assign(socket, %{game_state: state, question: question})}
   end
 end

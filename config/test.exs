a0b1f924491d3435cf63b84dd1cf384849cdee08
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-10-29 16:54:22 +0100

    Update code format

--- config/test.exs
+++ config/test.exs
@@ -8,8 +8,7 @@ config :workera_spawners, WorkeraSpawnersWeb.Endpoint,
   server: false
 
 # In test we don't send emails.
-config :workera_spawners, WorkeraSpawners.Mailer,
-  adapter: Swoosh.Adapters.Test
+config :workera_spawners, WorkeraSpawners.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
--- test/workera_spawners_web/controllers/error_json_test.exs
+++ test/workera_spawners_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule WorkeraSpawnersWeb.ErrorJSONTest do
   use WorkeraSpawnersWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert WorkeraSpawnersWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert WorkeraSpawnersWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do

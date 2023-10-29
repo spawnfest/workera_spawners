6533d5b6cc4ed36285d648721946bbdf97d17e8f
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-10-29 21:55:08 +0100

    Got the liveview working

--- lib/workera_spawners_web/live/trivia_live.ex
+++ lib/workera_spawners_web/live/trivia_live.ex
@@ -1,54 +1,76 @@
 defmodule WorkeraSpawnersWeb.TriviaLive do
+  alias WorkeraSpawners.GameServerTest
   use WorkeraSpawnersWeb, :live_view
 
   alias WorkeraSpawners.GameServer
 
   def mount(_params, _session, socket) do
-    server = GameServer.start_link(__MODULE__)
     {
       :ok,
       socket
       |> assign(
-      server: server,
-      game_state: :awaiting_name,
-      name: nil,
-      question: nil
+        game_state: GameServer.get_game_state(),
+        name: nil,
+        question: nil
       )
     }
   end
 
   def render(assigns) do
     ~H"""
-    <%= case @game_state do %>
-      <% :awaiting_name -> %>
+    <%= case {@game_state, @name, @question} do %>
+      <% {:awaiting_players, nil, _question} -> %>
         <%= form_for :name_form, "#", [phx_submit: :save_name], fn f -> %>
-          <%= text_input f, :name, placeholder: "Enter your name" %>
-          <%= submit "Submit" %>
+          <%= text_input(f, :name, placeholder: "Enter your name") %>
+          <%= submit("Submit") %>
         <% end %>
-      <% :awaiting_players -> %>
-        <!-- Waiting for server event... -->
-      <% :finished -> %>
-        <!-- Finished game... -->
-      <% _ -> %>
+      <% {_, nil, _} -> %>
+        <p>Game going on, please wait</p>
+      <% {:awaiting_players, _name, _question} -> %>
+        <p>Waiting for other players!</p>
+      <% {:awaiting_answers, _name, nil} -> %>
+        <p>Waiting for question!</p>
+      <% {:awaiting_answers, _name, question} -> %>
         <%= form_for :question_form, "#", [phx_submit: :save_answer], fn f -> %>
-          <label><%= @question %></label>
-          <%= text_input f, :answer, placeholder: "Enter your answer" %>
-          <%= submit "Submit" %>
+          <label><%= question.text %></label>
+          <%= text_input(f, :answer, placeholder: "Enter your answer") %>
+          <%= submit("Submit") %>
         <% end %>
+      <% {:finished, _name, _question} -> %>
+        <p>Game finished</p>
     <% end %>
     """
   end
 
-  def handle_event("save_name", %{"name_form" => %{"name" => name}}, %{assigns: %{server: s}} = socket) do
-    {:noreply, assign(socket, :name, name)}
+  def handle_event(
+        "save_name",
+        %{"name_form" => %{"name" => name}},
+        socket
+      ) do
+    case GameServer.add_player(name) do
+      :ok ->
+        {:noreply, assign(socket, game_state: GameServer.get_game_state(), name: name)}
+
+      _else ->
+        {:noreply, assign(socket, game_state: GameServer.get_game_state())}
+    end
   end
 
-  def handle_event("save_answer", %{"question_form" => %{"answer" => _answer}}, socket) do
-    # Handle answer submission
-    {:noreply, socket}
+  def handle_event("save_answer", %{"question_form" => %{"answer" => answer}}, socket) do
+    case GameServer.answer(answer) do
+      :correct ->
+        {:noreply, assign(socket, question: nil)}
+
+      :incorrect ->
+        {:noreply, socket}
+    end
   end
 
-  def handle_info({:question, question}, state, socket) do
-    {:noreply, assign(socket, %{game_state: state, question: question})}
+  def handle_info({:question, question}, socket) do
+    {:noreply, assign(socket, %{game_state: GameServer.get_game_state(), question: question})}
+  end
+
+  def handle_info({:finished, players}, socket) do
+    {:noreply, assign(socket, %{game_state: GameServer.get_game_state(), question: nil})}
   end
 end
